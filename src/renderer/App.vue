2e9c7a66d34caed8f3a26313dd92c6ab9e4aae99
Indexyz <jiduye@gmail.com>
2018-02-06 02:44:57 +0000

    init commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'burstsocks'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
